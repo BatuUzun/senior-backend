6edef0dfb7dc3b019e3c669cd03110be2d1fb5ef
BatuUzun <uzunbatu@hotmail.com>
2025-02-13 22:15:11 +0300

    favorite updated

---
+++ favorite/src/main/java/com/favorite/dto/FavoriteProfileResponseDTO.java
@@ -0,0 +1,22 @@
+package com.favorite.dto;
+
+import java.time.LocalDateTime;
+
+public class FavoriteProfileResponseDTO {
+    private Long id;
+    private String spotifyId;
+    private String type;
+    private LocalDateTime createdAt;
+
+    public FavoriteProfileResponseDTO(Long id, String spotifyId, String type, LocalDateTime createdAt) {
+        this.id = id;
+        this.spotifyId = spotifyId;
+        this.type = type;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() { return id; }
+    public String getSpotifyId() { return spotifyId; }
+    public String getType() { return type; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+}
