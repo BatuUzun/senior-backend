f971c912663a47bf8f5b904c59cc1751999f6a7c
BatuUzun <uzunbatu@hotmail.com>
2025-02-14 17:33:51 +0300

    user follow updated

--- userfollow/src/main/java/com/userfollow/userfollow/controller/UserFollowsController.java
+++ userfollow/src/main/java/com/userfollow/userfollow/controller/UserFollowsController.java
@@ -2,12 +2,14 @@ package com.userfollow.userfollow.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -88,5 +90,10 @@ public class UserFollowsController {
         } catch (ProxyServiceException e) {
             return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(e.getMessage());
         }
+    }
+    
+    @GetMapping("/{userId}/followed")
+    public ResponseEntity<Set<Long>> getFollowedUsers(@PathVariable Long userId) {
+        return ResponseEntity.ok(userFollowsService.getFollowedUsers(userId));
     }
 }
